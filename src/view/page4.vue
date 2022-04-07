56f459efe4c646a32816fd8c115b081a5a36df0d
CDownPace <53719402+CDownPace@users.noreply.github.com>
2022-04-07 15:53:12 +0800

    feat: 模块化

--- src/view/page4.vue
+++ src/view/page4.vue
@@ -5,19 +5,10 @@
   </div>
 </template>
 <script lang="ts">
-import { ref } from "vue";
+import {nowTime,getNowTime} from './hooks/useNowTime'
 const app = {
   name: "App",
   setup() {
-    const nowTime=ref("00:00:00");
-    const getNowTime=()=>{
-      const now=new Date();
-      const hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
-      const minu =now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
-      const sec =now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
-      nowTime.value = hour + ":" + minu + ":" + sec;
-      setTimeout(getNowTime, 1000);   //每一秒执行一次这个方法
-    }
     return{
       nowTime,getNowTime,
     }
